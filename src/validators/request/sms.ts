f22551c426af9151ad963eb4fdd6f65375868fee
André <matthiez@gmail.com>
2020-12-16 21:15:08 +0100

    validators.sms: fixed TTL, added label max and foreign_id checks

--- src/validators/request/sms.ts
+++ src/validators/request/sms.ts
@@ -2,25 +2,51 @@ import {SmsParams} from '../../types';
 import isValidDate from 'date-fns/isValid';
 import Util from '../../lib/Util';
 
+export const DEFAULT_PATTERN = /[0-9a-z-@_.]/i;
+
 export const DELAY_DATE_FORMAT = 'yyyy-mm-dd hh:ii';
 export const DELAY_PATTERN = /^([0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+
+export const FOREIGN_ID_MAX_LENGTH = 64;
+export const FOREIGN_ID_PATTERN = DEFAULT_PATTERN;
+
 export const FROM_ALLOWED_SPECIAL_CHARS =
     ['/', ' ', '.', '-', '@', '_', '!', '(', ')', '+', '$', ',', '&',];
 export const FROM_ALPHANUMERIC_MAX = 11;
 export const FROM_NUMERIC_MAX = 16;
-export const LABEL_PATTERN = /[0-9a-z-@_.]/i;
+
+export const LABEL_MAX_LENGTH = 100;
+export const LABEL_PATTERN = DEFAULT_PATTERN;
+
 export const TEXT_MAX_LENGTH = 1520;
-export const TTL_MAX = 86400000;
-export const TTL_MIN = 300000;
+
+export const TTL_MAX = Number.MAX_SAFE_INTEGER;
+export const TTL_MIN = 1;
 
 export default (params: SmsParams) => {
     delay(params.delay);
+    foreign_id(params.foreign_id);
     from(params.from);
     label(params.label);
     text(params.text);
     to(params.to);
     ttl(params.ttl);
 }
+
+export const foreign_id = (foreignId: SmsParams['foreign_id']) => {
+    if (!foreignId) {
+        return;
+    }
+
+    if (foreignId.match(FOREIGN_ID_PATTERN)) {
+        throw new Error(`foreign_id must match the regex pattern ${FOREIGN_ID_PATTERN}.`);
+    }
+
+    if (FOREIGN_ID_MAX_LENGTH < foreignId.length) {
+        throw new Error(
+            `The foreign_id cannot be longer than ${FOREIGN_ID_MAX_LENGTH} characters.`);
+    }
+};
 
 export const from = (from: SmsParams['from']) => {
     if (!from || !from.length) {
@@ -46,13 +72,17 @@ export const from = (from: SmsParams['from']) => {
 };
 
 export const label = (label: SmsParams['label']) => {
-    //TODO: max length?! there must be one.
     if (!label) {
         return;
     }
 
     if (label.match(LABEL_PATTERN)) {
         throw new Error(`label must match the regex pattern ${LABEL_PATTERN}.`);
+    }
+
+    if (LABEL_MAX_LENGTH < label.length) {
+        throw new Error(
+            `The label cannot be longer than ${LABEL_MAX_LENGTH} characters.`);
     }
 };
 
