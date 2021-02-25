08ecba6e72f225c3ca8f5e6bb22ab06cc4b22aa1
André Matthies <matthiez@gmail.com>
2021-02-25 11:36:28 +0100

    add webhook payload types

--- src/types/hooks.ts
+++ src/types/hooks.ts
@@ -1,5 +1,7 @@
 import {HooksAction} from '../constants/byEndpoint/hooks/HooksAction';
 import {HOOK_EVENT_TYPES, HOOK_REQUEST_METHODS} from '../constants/byEndpoint/hooks';
+import {AnyObject} from './index';
+import {StatusReportCode} from '../constants/byEndpoint/status/StatusReportCode';
 
 type HooksBaseParams<A extends HooksAction> = {
     action: A
@@ -60,3 +62,35 @@ export type HooksSubscribeErrorResponse = HooksSubscribeResponse<false>
 export type HooksUnsubscribeResponse = {
     success: boolean
 }
+
+export type HookAllPayloadEvent = 'dlr' | 'sms_mo' | 'voice_status'
+
+export type HookAllPayloadBase<Event extends HookAllPayloadEvent, Data extends AnyObject> = {
+    data: Data
+    webhook_event: Event
+    webhook_timestamp: string
+}
+
+export type HookAllPayloadInboundSms = HookAllPayloadBase<'sms_mo', {
+    id: number
+    sender: string
+    system: string
+    text: string
+    time: number
+}>
+
+export type HookAllPayloadSmsStatus = HookAllPayloadBase<'dlr', {
+    msg_id: string
+    status: StatusReportCode
+    timestamp: string
+}>
+
+export type HookAllPayloadVoiceStatus = HookAllPayloadBase<'voice_status', {
+    callerId: string
+    duration: string
+    id: string
+    pricePerMinute: number
+    recipient: string
+    status: 'completed' | string // TODO: add more statuses
+    timestamp: number
+}>
