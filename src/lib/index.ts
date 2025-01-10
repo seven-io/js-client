ed1bba9adf45920195b5ff03029a6eb61fa8a33f
André Matthies <matthiez@gmail.com>
2025-01-10 15:02:46 +0100

    add type Profile

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
 export * from './ErrorCode'
+export * from './oauth'
---
+++ src/lib/oauth.ts
@@ -0,0 +1,10 @@
+// noinspection JSUnusedGlobalSymbols
+
+export type Profile = {
+    alias: string
+    balance: string
+    company: string
+    email: string
+    success: boolean
+    user_id: number
+}
