c67b3c37a4ccd44d8703159934e890bc18d999f1
André Matthies <matthiez@gmail.com>
2025-01-22 13:22:01 +0100

    add type TokenResponse

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -8,3 +8,14 @@ export type Profile = {
     success: boolean
     user_id: number
 }
+
+export type OauthScope = 'analytics' | 'balance' | 'contacts' | 'hooks' | 'journal' | 'lookup' | 'pricing' | 'sms'
+    | 'status' | 'subaccounts' | 'validate_for_voice' | 'voice'
+
+export type TokenResponse = {
+    access_token: string
+    expires_in: number
+    refresh_token: string
+    scope: OauthScope[]
+    token_type: 'Bearer'
+}
